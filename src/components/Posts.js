fb8e99359020170d5727b64f423d8613e51c13e3
thiyari <manikanth578@gmail.com>
2025-07-13 20:06:29 +0530

    update

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -9,6 +9,13 @@ class Posts extends Component {
     this.props.fetchPosts()
   }
 
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.newPost) {
+      this.props.posts.unshift(nextProps.newPost)
+    }
+  }
+
 /*
   constructor(props){
     super(props);
@@ -44,11 +51,13 @@ class Posts extends Component {
 
 Posts.propTypes = {
   fetchPosts: PropTypes.func.isRequired,
-  posts: PropTypes.array.isRequired
+  posts: PropTypes.array.isRequired,
+  newPost: PropTypes.object
 }
 
 const mapStateToProps = state => ({
-  posts: state.posts.items
+  posts: state.posts.items,
+  newPost: state.posts.item
 })
 
 //export default Posts
