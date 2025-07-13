0345060c4800f72cb8bc5c020093b5172d2acd29
thiyari <manikanth578@gmail.com>
2025-07-13 20:09:35 +0530

    update

--- src/store.js
+++ src/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducers';
-//import { compose } from 'redux';
 
 const store = configureStore({
   reducer:  rootReducer,
@@ -8,8 +7,6 @@ const store = configureStore({
     getDefaultMiddleware({
       thunk: true, // explicitly enable (still not required)
     }),
-  //enhancers: (defaultEnhancer) =>
-  //compose(defaultEnhancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()),
 });
 
 export default store;
